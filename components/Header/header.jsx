a931338efc53637766a5d04cc611d0ef40195764
Maruf Hossain <mdmarufh974@gmail.com>
2024-07-07 00:58:37 +0600

    navber complete

--- components/Header/header.jsx
+++ components/Header/header.jsx
@@ -6,15 +6,15 @@ const Header = () => {
         <div className="bg-[#b6b6b679] ">
             <div className="max-w-7xl mx-auto py-5 flex items-center justify-between">
                 <Link href={'/'}><h2 className="text-3xl font-semibold"><span className="text-orange-400 font-bold">B</span>log.</h2></Link>
-                <div>
-                    <ul className="flex gap-5">
-                        <li>Blogs</li>
-                        <li>Blogs</li>
-                        <li>Blogs</li>
+                <div className="flex gap-10 items-center">
+                    <ul className="flex gap-5 text-xl">
+                        <li><Link href={'/blogs'}>Blogs</Link></li>
+                        <li><Link href={'/about'}>About</Link></li>
+                        <li><Link href={'/contact'}>Contact</Link></li>                     
                     </ul>
                     <form className="flex">
-                        <input type="email" name="email" id="" />
-                        <input type="submit" value="Subscribe" />
+                        <input className="px-4 py-2 rounded-l border border-red-300 focus:outline-none" type="email" name="email" id="" />
+                        <input className="px-5 py-2 rounded-r bg-red-300 text-[#3a3a3a] font-medium hover:bg-red-400 hover:cursor-pointer" type="submit" value="Subscribe" />
                     </form>
                 </div>
             </div>
